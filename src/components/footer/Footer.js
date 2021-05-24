edbc4f8a4ea9c632e71cb59c290faa872d2da027
rosemary.ogbonna <rosemary.ogbonna@turnkeyafrica.com>
2021-05-24 05:49:52 -0500

    --no-amend

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -8,15 +9,15 @@ const Footer = () => {
                     <div className="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop">
                   <span className="tx-14"
                   >Copyright © 2020
-                    <a href="#" target="_blank"> Kechyy todo application</a>. All
+                    <Link to="/" target="_blank"> Kechyy todo application</Link>. All
                     rights reserved.</span>
                     </div>
                     <div
                         className="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop d-flex justify-content-end">
                         <div className="d-flex align-items-center">
-                            <a href="">Documentation</a>
+                            <Link to="/">Documentation</Link>
                             <span className="vertical-divider"></span>
-                            <a href="">FAQ</a>
+                            <Link to="/">FAQ</Link>
                         </div>
                     </div>
                 </div>
